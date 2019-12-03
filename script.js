e93e2b89dfa00f78c4ab8824b76cdd936d964485
KennethMokhethi <kennethmahlatsemokhethi@gmail.com>
2019-12-03 15:44:56 +0200

    Created add function and list

--- script.js
+++ script.js
@@ -12,10 +12,30 @@ pool.connect(function(err) {
   else console.log("Connected!");
 });
 
-pool.query(
-  "INSERT INTO Visiters(Name,Age,Date_of_visit,Time_of_visit,Name_of_the_person_who_assisted_the_visitor,Comments)VALUES('Buhle',24,'23:12:2019','12:07','Sindy','None')",
-  (err, res) => {
+function add_New_visitor(
+  argName,
+  argAge,
+  argDate_of_visit,
+  argTime_of_visit,
+  argName_of_person_who_assisted_the_visitor,
+  argComments
+) {
+  pool.query(
+    "INSERT INTO Visiters(Name,Age,Date_of_visit,Time_of_visit,Name_of_the_person_who_assisted_the_visitor,Comments)VALUES" +
+      `(${argName},${argAge},${argDate_of_visit},${argTime_of_visit},${argName_of_person_who_assisted_the_visitor},${argComments})`,
+    (err, res) => {
+      console.log(err, res);
+      pool.end();
+    }
+  );
+}
+
+function list_all_visitor() {
+  pool.query("SELECT * from Visiters", (err, res) => {
     console.log(err, res);
     pool.end();
-  }
-);
+  });
+}
+
+// add_New_visitor("Buhles", 19, "23", "12", "Nelly", "Thanks");
+list_all_visitor();
